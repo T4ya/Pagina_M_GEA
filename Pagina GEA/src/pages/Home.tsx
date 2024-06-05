b99c70e161dcee524d5fa26aca57a989441fcc91
T4ya <yefergnz@gmail.com>
2024-06-04 20:17:57 -0500

    Primer Comit

---
+++ Pagina GEA/src/pages/Home.tsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h1>Bienvenido al home</h1>
+      <section className="bg-white-500/60 border-2 m-10 justify-center text-center rounded-md p-20 shadow-sm ">Este es una card</section>
+    </div>
+  )
+}
+
+export default Home
